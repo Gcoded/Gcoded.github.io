890809883e3cbfdcd204018354d3071ff76af47f
Godfrey <godfreyjr@gmail.com>
2021-01-16 13:21:17 -0600

    Updated year in footer and padding of project text

--- js/scripts.js
+++ js/scripts.js
@@ -54,3 +54,8 @@
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
 })(jQuery); // End of use strict
+
+
+// Get year for footer
+const footerYear = document.getElementById('year');
+footerYear.textContent = new Date().getFullYear();
